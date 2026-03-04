35604c00c3c3b09c0256bfe246544d77f667f271
rhenzarg <rhenzarg@gmail.com>
2026-03-04 18:54:54 +0700

    Praktikum3_Percobaan3_Pertanyaan2

---
+++ Praktikum03/Matakuliah28.java
@@ -0,0 +1,20 @@
+package Praktikum03;
+
+public class Matakuliah28 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah28() {
+
+    }
+
+    public void tambahData(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+}
---
+++ Praktikum03/MatakuliahDemo28.java
@@ -0,0 +1,41 @@
+package Praktikum03;
+
+import java.util.Scanner;
+
+public class MatakuliahDemo28 {
+    public static void main(String[] args) {
+        Scanner rhenza = new Scanner(System.in);
+        Matakuliah28[] arrayMatakuliah28 = new Matakuliah28[3];
+
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i < 3; i++) {
+            arrayMatakuliah28[i] = new Matakuliah28();
+            System.out.println("Masukan Data Mata Kuliah ke-" + (i+1));
+            System.out.print("Kode        : ");
+            kode = rhenza.nextLine();
+            System.out.print("Nama        : ");
+            nama = rhenza.nextLine();
+            System.out.print("Sks         : ");
+            dummy = rhenza.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam  : ");
+            dummy = rhenza.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("-------------------------------");
+
+            arrayMatakuliah28[i].tambahData(kode, nama, sks, jumlahJam);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Mata Kuliah ke-" + (i+1));
+            System.out.println("Kode        : " + arrayMatakuliah28[i].kode);
+            System.out.println("Nama        : " + arrayMatakuliah28[i].nama);
+            System.out.println("Sks         : " + arrayMatakuliah28[i].sks);
+            System.out.println("Jumlah Jam  : " + arrayMatakuliah28[i].jumlahJam);
+            System.out.println("-------------------------------");
+        }
+        rhenza.close();
+    }
+}
